cc288f364462347fd33207583b5fa1f867477ade
vbirukov <vbirukov@gmail.com>
2017-04-10 15:24:12 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,15 +2,23 @@
  * Created by concisv on 25.12.2016.
  */
 
-let elem = document.querySelector(".clps");
+let elem = $(".clps");
 if(elem) {
-  let message = 'Свернуть текст';
-  elem.addEventListener("click", function () {
-    let messageTmp = elem.innerText;
-    elem.innerText = message;
+  let message = 'Свернуть';
+  let messageDefault = '';
+  elem.on("click", function () {
+    let messageTmp = this.innerText;
+    this.innerText = message;
     message = messageTmp;
   });
 }
+
+// $('.clps').on('click', function () {
+//   let message = 'Свернуть текст';
+//   let messageTmp = $('.clps').text();
+//   $('.clps').text(message);
+//   message = messageTmp;
+// })
 
 
 let modalWindow = $('#myModal');
@@ -23,9 +31,15 @@ $('#sendRequestBtn').on('click', function () {
 
 $('#sendRequestPortfolioBtn').on('click', function () {
   $('#portfolioCard').modal('hide').on('hidden.bs.modal', function (e) {
-    $('#thankYouWindow').modal('show');
+   // $('#thankYouWindow').modal('show');
   });
 });
+
+// $('#sendRequestPortfolioBtn').on('click', function () {
+//   $('#portfolioCard').modal('hide').on('hidden.bs.modal', function (e) {
+//     $('#thankYouWindow').modal('show');
+//   });
+// });
 
 
 $('.picture_box > div').on('click', function () {
@@ -43,11 +57,45 @@ if($('#portfolioCollapseBtn')) {
   });
 }
 
+
+//cities selector in upper right corner
+//
 $('#cities_select_menu li').on('click', function () {
-  console.log('DBG    inner text: ' + $('#currentCity').text().substr(7));
   let bufferValue =  $('#currentCity').text().substr(7);
-  console.log('DBG this.innerText: ' + this.innerText);
   $('#currentCity').html('<i class="material-icons">near_me</i>' + this.innerText);
   this.innerHTML = '<a class="nav-link" href="#">' + bufferValue +'</a>';
 });
 
+
+$( function(){// Get the modal
+    var modal = document.getElementById('my-Modal');
+
+    // Get the button that opens the modal
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    // $('.bigRedSlider img').on('click', function() {
+    //     modal.style.display = "block";
+    //     console.log('DBG: ' + JSON.stringify($('this')));
+    //     $('.my-modal-content img').attr('src', this.src)
+    //     $('body').css('overflow', 'hidden');
+    // })
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        $('body').css('overflow', 'visible');
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            $('body').css('overflow', 'visible');
+        }
+
+    }
+});
